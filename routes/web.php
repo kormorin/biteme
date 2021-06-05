48af1e141ec6cf6844b801ea4e5bcbc5510e029f
Klein Ádám <adam.klein@erba.hu>
2021-06-05 22:22:06 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    dd(config('app.url'))
+    dd(config('app.url'));
 });
 
 Route::get('menu', 'MenuController@daySelection')->name('menu');
