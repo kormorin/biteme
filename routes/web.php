8127283e35edc93cc51e859b7628897cb910f762
Klein Ádám <adam.klein@erba.hu>
2021-06-05 22:23:59 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+/*
 
 Route::get('/', function () {
     dd(config('app.url'));
 });
-
+*/
 Route::get('menu', 'MenuController@daySelection')->name('menu');
 
 Route::domain('portal.' . config('app.url'))->group(function() {
