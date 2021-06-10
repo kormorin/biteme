d417e23e23b5c6cf6f39aa086a116da95e8552c6
Klein Ádám <adam.klein@erba.hu>
2021-06-10 21:15:03 +0200

    wip

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -69,8 +69,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('menu')" :active="request()->routeIs('dashboard')">
-                {{ __('navigation.menu_creation') }}
+            <x-responsive-nav-link :href="route('profile')" :active="request()->routeIs('profile')">
+                {{ __('Profile') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('menu')" :active="request()->routeIs('menu')">
+                {{ __('Menus') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('orders')" :active="request()->routeIs('orders')">
+                {{ __('Orders') }}
             </x-responsive-nav-link>
         </div>
 
